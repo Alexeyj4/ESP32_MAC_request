8c7e9561968dfce59042d52e7b2ceff6eeec8341
Alexey <alexeyj4@yandex.ru>
2024-05-09 17:31:12 +0300

    Update ESP32_MAC_request.ino

--- ESP32_MAC_request/ESP32_MAC_request.ino
+++ ESP32_MAC_request/ESP32_MAC_request.ino
@@ -1,14 +1,15 @@
-#ifdef ESP32
+//#ifdef ESP32
 #include <WiFi.h>
-#else 
-#include <ESP8266WiFi.h>
-#endif
+//#else 
+//#include <ESP8266WiFi.h>
+//#endif
  
 void setup() { 
-  Serial.begin(9600);
-  Serial.println(); 
-  Serial.print("ESP Board MAC Address:&nbsp; "); 
-  Serial.println(WiFi.macAddress());
+  Serial.begin(115200);
+
 }
-void loop() {
+void loop() {   
+  //Serial.print("ESP Board MAC Address:&nbsp; "); 
+  Serial.println(WiFi.macAddress());
+  delay(1000); 
 }
